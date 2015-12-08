15090eb680343e2d5bac154bc4f66474d8c46882
soukainasalihi <souka-free1@hotmail.fr>
2015-12-08 15:13:13 -0500

    Update ConnecToDB.java

--- userLogin/src/ConnectToDB/ConnecToDB.java
+++ userLogin/src/ConnectToDB/ConnecToDB.java
@@ -18,23 +18,25 @@ public class ConnecToDB {
     //Create a constructor
     public ConnecToDB()
     {
-        //Database url, database location in the host. use localhost if the database in your computer. 
+        //Database url, database location in the host. Use localhost if the database in your computer. 
         String url="jdbc:mysql://localhost:3306/BHCCgame"; 
         String username="root";
-        String password="";// password in your computer
+        String password="password";// password in your computer
         
         try {
-            
+            // connect to the Driver Manager using personal username and password for the localhost
             Class.forName("com.mysql.jdbc.Driver");
             connect=DriverManager.getConnection(url,username,password);
             stm=connect.createStatement();
            
         }catch (SQLException | ClassNotFoundException  ex)
         {
+            // Error message if connection is failed 
             System.out.println(ex.getMessage());
         }
     }
 
+// getters and setters to get the software transactional memory (STM) and connection
     public Connection getConnect() {
         return connect;
     }
