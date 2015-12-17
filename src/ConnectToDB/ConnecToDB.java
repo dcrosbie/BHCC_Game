43c7b52f44e28608fc5d8e70cfba4aeadeda149a
soukainasalihi <souka-free1@hotmail.fr>
2015-12-17 00:05:28 -0500

    Update ConnecToDB.java

--- src/ConnectToDB/ConnecToDB.java
+++ src/ConnectToDB/ConnecToDB.java
@@ -21,36 +21,37 @@ public class ConnecToDB {
         //Database url, database location in the host. use localhost if the database in your computer. 
         String url="jdbc:mysql://173.194.235.163:3306/BHCCgame"; 
         String username="scott";
-        String password="tiger";//password in your computer
+        String password="tiger";// or password in your computer
         
         try {
-            
+            // connect to the driver manager
             Class.forName("com.mysql.jdbc.Driver");
             connect=DriverManager.getConnection(url,username,password);
             stm=connect.createStatement();
-           
-        }catch (SQLException | ClassNotFoundException  ex)
+        }
+        // Catch execetion and print an error message
+        catch (SQLException | ClassNotFoundException  ex)
         {
             System.out.println(ex.getMessage());
         }
     }
-
+// get connection
     public Connection getConnect() {
         return connect;
     }
-
+// set connection
     public void setConnect(Connection connect) {
         this.connect = connect;
     }
-
+// get stm (software transactional memory)
     public Statement getStm() {
         return stm;
     }
-
+// set stem (software transactional memory)
     public void setStm(Statement stm) {
         this.stm = stm;
     }
 
-   
+  
     
 }
